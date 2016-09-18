8b61dd6bbaba991b212726f786cc3e03bb0a8a79
umit <i.umitakgun@gmail.com>
2016-09-18 23:37:50 +0300

    initial Commit

---
+++ DesignPatternsSwift/DesignPatternsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignPatternsSwift
+//
+//  Created by umit on 9/18/16.
+//  Copyright © 2016 umit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
